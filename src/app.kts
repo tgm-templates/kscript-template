ce069f7ab2684212ed74b86df438d61872180f6e
Libing Chen <libing.chen@gmail.com>
2020-11-21 14:08:47 -0800

    add  DEPS of dotenv-kotlin

--- src/app.kts
+++ src/app.kts
@@ -1,5 +1,7 @@
 #!/usr/bin/env kscript
 //DEPS khttp:khttp:1.0.0
+//DEPS io.github.cdimascio:dotenv-kotlin:6.2.1
+
 import khttp.get
 
 val ip = get("http://httpbin.org/ip").jsonObject.getString("origin")
